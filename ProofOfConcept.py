73bdec9670aa6418c5c52ee3fb7d10bb9de7cc85
Darío Clavijo <clavijodario@gmail.com>
2021-05-05 00:08:31 -0300

    black style

--- ProofOfConcept.py
+++ ProofOfConcept.py
@@ -30,15 +30,15 @@
 
 import hashlib
 
-b58_digits = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+b58_digits = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 tx = "9ec4bc49e828d924af1d1029cacf709431abbde46d59554b62bc270e3b29c4b1"
 p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
 # r  = 0xd47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad1
 # s1 = 0x44e1ff2dfd8102cf7a47c21d5c9fd5701610d04953c6836596b4fe9dd2f53e3e
 # s2 = 0x9a5f1c75e461d7ceb1cf3cab9013eb2dc85b6d0da8c3c6e27e3a5a5b3faa5bab
-z1 = 0xc0e2d0a89a348de88fda08211c70d1d7e52ccef2eb9459911bf977d587784c6e
-z2 = 0x17b0f41c8c337ac1e18c98759e83a8cccbc368dd9d89e5f03cb633c265fd0ddc
+z1 = 0xC0E2D0A89A348DE88FDA08211C70D1D7E52CCEF2EB9459911BF977D587784C6E
+z2 = 0x17B0F41C8C337AC1E18C98759E83A8CCCBC368DD9D89E5F03CB633C265FD0DDC
 
 # r1 and s1 are contained in this ECDSA signature encoded in DER (openssl default).
 der_sig1 = "3044"
@@ -52,21 +52,21 @@ der_sig2 += "0220d47ce4c025c35ec440bc81d99834a624875161a26bf56ef7fdc0f5d52f843ad
 der_sig2 += "02209a5f1c75e461d7ceb1cf3cab9013eb2dc85b6d0da8c3c6e27e3a5a5b3faa5bab"
 der_sig2 += "01"
 
-params = {'p': p, 'sig1': der_sig1, 'sig2': der_sig2, 'z1': z1, 'z2': z2}
+params = {"p": p, "sig1": der_sig1, "sig2": der_sig2, "z1": z1, "z2": z2}
 
 
 def hexify(s, flip=False):
     if flip:
-        return s[::-1].encode('hex')
+        return s[::-1].encode("hex")
     else:
-        return s.encode('hex')
+        return s.encode("hex")
 
 
 def unhexify(s, flip=False):
     if flip:
-        return s.decode('hex')[::-1]
+        return s.decode("hex")[::-1]
     else:
-        return s.decode('hex')
+        return s.decode("hex")
 
 
 def dhash(s):
@@ -74,7 +74,7 @@ def dhash(s):
 
 
 def rhash(s):
-    h1 = hashlib.new('ripemd160')
+    h1 = hashlib.new("ripemd160")
     h1.update(hashlib.sha256(s).digest())
     return h1.digest()
 
@@ -84,11 +84,11 @@ def base58_encode(n):
     while n > 0:
         n, r = divmod(n, 58)
         tmp.insert(0, (b58_digits[r]))
-    return ''.join(tmp)
+    return "".join(tmp)
 
 
 def base58_encode_padded(s):
-    res = base58_encode(int('0x' + s.encode('hex'), 16))
+    res = base58_encode(int("0x" + s.encode("hex"), 16))
     pad = 0
     for c in s:
         if c == chr(0):
@@ -105,10 +105,10 @@ def base58_check_encode(s, version=0):
 
 
 def get_der_field(i, binary):
-    if (ord(binary[i]) == 2):
-        length = binary[i+1]
+    if ord(binary[i]) == 2:
+        length = binary[i + 1]
         end = i + ord(length) + 2
-        string = binary[i+2:end]
+        string = binary[i + 2 : end]
         return string
     else:
         return None
@@ -118,7 +118,7 @@ def get_der_field(i, binary):
 def der_decode(hexstring):
     binary = unhexify(hexstring)
     full_length = ord(binary[1])
-    if ((full_length + 3) == len(binary)):
+    if (full_length + 3) == len(binary):
         r = get_der_field(2, binary)
         s = get_der_field(len(r) + 4, binary)
         return r, s
@@ -131,9 +131,9 @@ def show_results(privkeys):
     for privkey in privkeys:
         print("intPrivkey = %d" % privkey)
         print("hexPrivkey = %064x" % privkey)
-        wif = base58_check_encode(hexprivkey.decode('hex'), version=128)
+        wif = base58_check_encode(hexprivkey.decode("hex"), version=128)
         print("bitcoin Privkey (WIF) = %s" % wif)
-        wif = base58_check_encode((hexprivkey + "01").decode('hex'), version=128)
+        wif = base58_check_encode((hexprivkey + "01").decode("hex"), version=128)
         print("bitcoin Privkey (WIF compressed) = %s" % wif)
 
 
@@ -146,12 +146,14 @@ def show_params(params):
 
 
 def inverse_mult(a, b, p):
-    """ By the Fermat's little theorem we can say that:
+    """By the Fermat's little theorem we can say that:
     a * pow(b,p-2,p) % p is the same as (a/b mod p)
     This is needed to avoid floating numbers since we are dealing with prime numbers
     and beacuse this the python built in division isn't suitable for our needs,
     it returns floating point numbers rounded and we don't want them."""
-    y = (a * pow(b, p - 2, p)) % p  # (pow(a, b) modulo p) where p should be a prime number
+    y = (
+        a * pow(b, p - 2, p)
+    ) % p  # (pow(a, b) modulo p) where p should be a prime number
     return y
 
 
@@ -161,7 +163,7 @@ def derivate_privkey(p, r, s1, s2, z1, z2):
 
     s1ms2 = s1 - s2
     s1ps2 = s1 + s2
-    ms1ms2 = - s1 - s2
+    ms1ms2 = -s1 - s2
     ms1ps2 = -s1 + s2
     z1ms2 = z1 * s2
     z2ms1 = z2 * s1
@@ -186,26 +188,26 @@ def derivate_privkey(p, r, s1, s2, z1, z2):
 
 def process_signatures(params):
 
-    p = params['p']
-    sig1 = params['sig1']
-    sig2 = params['sig2']
-    z1 = params['z1']
-    z2 = params['z2']
+    p = params["p"]
+    sig1 = params["sig1"]
+    sig2 = params["sig2"]
+    z1 = params["z1"]
+    z2 = params["z2"]
 
     tmp_r1, tmp_s1 = der_decode(sig1)  # Here we extract r and s from the DER signature.
     tmp_r2, tmp_s2 = der_decode(sig2)  # Idem.
 
     # the key of ECDSA are the integer numbers thats why we convert hexa from to them.
-    r1 = int(tmp_r1.encode('hex'), 16)
-    r2 = int(tmp_r2.encode('hex'), 16)
-    s1 = int(tmp_s1.encode('hex'), 16)
-    s2 = int(tmp_s2.encode('hex'), 16)
+    r1 = int(tmp_r1.encode("hex"), 16)
+    r2 = int(tmp_r2.encode("hex"), 16)
+    s1 = int(tmp_s1.encode("hex"), 16)
+    s2 = int(tmp_s2.encode("hex"), 16)
 
     # If r1 and r2 are equal the two signatures are weak
     # and we can recover the private key.
 
-    if (r1 == r2):
-        if (s1 != s2):  # This:(s1-s2)>0 should be complied in order be able to compute.
+    if r1 == r2:
+        if s1 != s2:  # This:(s1-s2)>0 should be complied in order be able to compute.
             privkey = derivate_privkey(p, r1, s1, s2, z1, z2)
             return privkey
         else:
